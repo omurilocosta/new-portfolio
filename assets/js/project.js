6a7a844088e4f7be8fe3904decbb55bfcb67b387
Murilo Costa <murilocosta2707@gmail.com>
2025-02-14 17:10:23 -0300

    Add files via upload

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,10 +1,20 @@
 let projJSON = [
     {
         id: 1,
+        img: 'https://lh3.googleusercontent.com/d/1B0DKA0L_RUVmIWlrz8cmnT-AYqNWMHEg',
         name: "PIPOBECA",
-        description: "This is project 1",
+        desc: "Bem-vindo ao repositório do site da Pipobeca, uma loja de pipocas gourmet! Este projeto foi desenvolvido com o objetivo de praticar e aprimorar minhas habilidades de programação front-end. A loja Pipobeca Gourmet realmente existe.",
         linkGit: "https://github.com/omurilocosta/PipoBeca/tree/main",
-        linkDemo: "https://pipobeca.netlify.app",
-        languages: ['html', 'css','js']
+        linkDep: "https://pipobeca.netlify.app",
+        techs: ['html', 'css','js']
+    },
+    {
+        id: 1,
+        img: 'https://lh3.googleusercontent.com/d/1Op8Lxc25Mts2K9HZDtHJaeEC2556u7UC',
+        name: "CLONE SPOTIFY",
+        desc: "Este repositório contém o projeto desenvolvido durante a Imersão Front-End da Alura, onde criei uma página inspirada no Spotify.",
+        linkGit: "https://github.com/omurilocosta/spotify-imersao-alura",
+        linkDep: "https://omurilocosta.github.io/spotify-imersao-alura/",
+        techs: ['html', 'css','js']
     }
 ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,51 @@
+// Variaveis para encurtar códigos.
 const slct = (element) => document.querySelector(element)
 const slctA = (elements) => document.querySelectorAll(elements)
 
-
+// Variavel para elemento do doc.
 const html = slct('html')
-const config = slct('#config')
-const navbar = slct('#navbar')
-const check = slct('#darkmode')
-const close = slct('#close')
+const dark = slct('#darkmode')
 
-
-
-const fillProjInfos = (projItem, item, index) => {
-    projItem.setAttribute('data-key', index)
-    projItem.querySelector('.project-item--title').innerHTML = item.name
+let names = slctA('.logo')
+for (i=0;i<(names.length);i++){
+    names[i].innerText = '<MC>'
 }
 
+dark.addEventListener('click', () => {
+    html.classList.toggle('dark-mode')
+})
 
 
+// ! THIS IS THE PROJECT AREA SCRIPT
 
+const iconTechs = {
+    html: "<div class='techs'><i class='bx bxl-html5'></i><p>HTML</p></div>",
+    css: "<div class='techs'><i class='bx bxl-css3'></i><p>CSS</p></div>",
+    js: "<div class='techs'><i class='bx bxl-javascript'></i><p>JS</p></div>"
+}
 
+// FUNCTION TO CONVERT WORD TO ICON
+const convert_to_icon = (icon) => {return iconTechs[icon]}
+
+// FUNCTION TO FILL INFO PROJECT
+const fill_info_project = (projItem, item, index) => {
+    projItem.querySelector('.project-item--img a').href = item.linkDep
+    projItem.querySelector('.project-item--img a img').src = item.img
+    projItem.querySelector('.project-item--info .project-item--title').innerText = item.name
+    projItem.querySelector('.project-item--info .project-item--links #git').href = item.linkGit
+    projItem.querySelector('.project-item--info .project-item--links #dep').href = item.linkDep
+    projItem.querySelector('.project-item--info .project-item--desc').innerText = item.desc
+    for( i = 0; i < (item.techs.length); i++){
+        projItem.querySelector('.project-item--info .project-item--techs').innerHTML += convert_to_icon(item.techs[i])
+    }
+}
 
 projJSON.map((item,index) => {
-    //console.log(item,index);
-    let projItem = document.querySelector('.model .project-item').cloneNode(true)
+    let projItem = slct('.model .project-item').cloneNode(true)
     
-
     slct('.project-area').append(projItem)
 
-    fillProjInfos(projItem,item,index)
-    
+    fill_info_project(projItem, item, index)
+
 })
+// ! END PROJECT AREA SCRIPT
