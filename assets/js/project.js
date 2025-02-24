9b535f289d9d85451e35bb858a7e3e7c913b2218
Murilo Costa <murilocosta2707@gmail.com>
2025-02-24 19:52:29 -0300

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -9,12 +9,21 @@ let projJSON = [
         techs: ['html', 'css','js']
     },
     {
-        id: 1,
+        id: 2,
         img: 'https://lh3.googleusercontent.com/d/1Op8Lxc25Mts2K9HZDtHJaeEC2556u7UC',
         name: "CLONE SPOTIFY",
         desc: "Este repositório contém o projeto desenvolvido durante a Imersão Front-End da Alura, onde criei uma página inspirada no Spotify.",
         linkGit: "https://github.com/omurilocosta/spotify-imersao-alura",
         linkDep: "https://omurilocosta.github.io/spotify-imersao-alura/",
         techs: ['html', 'css','js']
+    },
+    {
+        id: 3,
+        img: 'https://lh3.googleusercontent.com/d/1JSsAzF2csaMM3L1LPrrhSNW9UpmRrJQD',
+        name: 'PalavraX',
+        desc: 'Este projeto é um jogo de advinhação para jogar com seus amigos',
+        linkGit: 'https://github.com/omurilocosta/palavrax',
+        linkDep: 'https://palavrax.wuaze.com',
+        techs: ['php', 'css', 'js', 'bd']
     }
 ]
