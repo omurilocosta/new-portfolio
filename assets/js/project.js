6c4286938eb67941efd7163b72dc47384c1afabd
Murilo Costa <murilocosta2707@gmail.com>
2025-02-27 21:15:55 -0300

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -10,6 +10,15 @@ let projJSON = [
     },
     {
         id: 2,
+        img: 'https://lh3.googleusercontent.com/d/1JSsAzF2csaMM3L1LPrrhSNW9UpmRrJQD',
+        name: 'PALAVRAX',
+        desc: 'PalavraX é um jogo de raciocínio e dedução em que você deve descobrir a palavra secreta com base em uma série de dicas progressivas. Quanto menos dicas utilizar, maior será a sua pontuação!O jogo combina lógica, conhecimento geral e um toque de mistério para testar suas habilidades. Com uma mecânica envolvente e dinâmica, PalavraX é perfeito para jogar com seus amigos para ver quem consegue adivinhar as palavras mais rápido.',
+        linkGit: 'https://github.com/omurilocosta/palavrax',
+        linkDep: 'http://palavrax.wuaze.com/',
+        techs: ['php', 'css', 'js', 'bd']
+    },
+    {
+        id: 3,
         img: 'https://lh3.googleusercontent.com/d/1Op8Lxc25Mts2K9HZDtHJaeEC2556u7UC',
         name: "CLONE SPOTIFY",
         desc: "Este repositório contém o projeto desenvolvido durante a Imersão Front-End da Alura, onde criei uma página inspirada no Spotify.",
@@ -17,13 +26,5 @@ let projJSON = [
         linkDep: "https://omurilocosta.github.io/spotify-imersao-alura/",
         techs: ['html', 'css','js']
     },
-    {
-        id: 3,
-        img: 'https://lh3.googleusercontent.com/d/1JSsAzF2csaMM3L1LPrrhSNW9UpmRrJQD',
-        name: 'PALAVRAX',
-        desc: 'PalavraX é um jogo de raciocínio e dedução em que você deve descobrir a palavra secreta com base em uma série de dicas progressivas. Quanto menos dicas utilizar, maior será a sua pontuação!O jogo combina lógica, conhecimento geral e um toque de mistério para testar suas habilidades. Com uma mecânica envolvente e dinâmica, PalavraX é perfeito para jogar com seus amigos para ver quem consegue adivinhar as palavras mais rápido.',
-        linkGit: 'https://github.com/omurilocosta/palavrax',
-        linkDep: 'http://palavrax.wuaze.com/',
-        techs: ['php', 'css', 'js', 'bd']
-    }
+    
 ]
